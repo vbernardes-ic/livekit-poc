34c0f31fe1d992d0ed9243e51d904014f9c2c806
António Alvarenga <antonioalvarengavd@gmail.com>
2023-01-02 19:14:16 +0000

    comment out rev ai lines

--- backend/wss.js
+++ backend/wss.js
@@ -7,21 +7,21 @@ wss.on("connection", function connection(ws) {
 
     // Connect to rev.ai websocket api
     // send binary data
-    const rev_ai_conn = new WebSocket('ws://www.rev.ai/path');
-    rev_ai_conn.on('open', function open() {
-        console.log("Connection with rev.ai estabilished")
-    });
+    // const rev_ai_conn = new WebSocket('ws://www.rev.ai/path');
+    // rev_ai_conn.on('open', function open() {
+    //     console.log("Connection with rev.ai estabilished")
+    // });
 
     // Messages sent from Egress
     ws.on("message", function message(data) {
         console.log("received: %s", data);
 
 
-        rev_ai_conn.send(array);
+        // rev_ai_conn.send(array);
     });
 
     // Messages sent from rev.ai
-    rev_ai_conn.on('message', function message(data) {
-        console.log('received: %s', data);
-    });
+    // rev_ai_conn.on('message', function message(data) {
+    //     console.log('received: %s', data);
+    // });
 });
