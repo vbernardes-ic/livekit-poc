2ee3b7accafce47337d86172045b9abc0c6d1e4b
Rui Abreu <rabreu@imaginarycloud.com>
2023-01-05 15:25:13 +0100

    Transcription Service managed by docker

---
+++ backend/transcription-service/my_whisper_service.py
@@ -0,0 +1,29 @@
+import whisper
+import json
+from tempfile import NamedTemporaryFile
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# load model and processor
+model = whisper.load_model('tiny')
+
+
+@app.route('/heartbeat', methods=['GET'])
+def heartbeat():
+    return "Ok"
+
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    audio_data = request.get_data()
+
+    with NamedTemporaryFile() as f:
+        f.write(audio_data)
+        transcript = model.transcribe(f.name)
+        return json.dumps({'transcription': transcript})
+
+
+if __name__ == '__main__':
+    print("Running...")
+    app.run(host="0.0.0.0",port=5050)
