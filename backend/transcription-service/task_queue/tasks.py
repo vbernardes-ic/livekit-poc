3cf8cd7523e67d269bb8cfbe59bf163eae735cf3
Vitor Bernardes <vitor@filespin.io>
2023-01-12 15:03:14 +0000

    Compute transcription execution time and change API call return value.

--- backend/transcription-service/task_queue/tasks.py
+++ backend/transcription-service/task_queue/tasks.py
@@ -4,13 +4,18 @@ import whisper
 import logging
 from base64 import b64decode
 from .celery import app
+import time
+from celery.signals import task_prerun, task_postrun
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 # Set up transcription model
 model = whisper.load_model('tiny.en')
+
+# To get task execution times
+tasks_exec_time = {}
 
 
 @app.task
@@ -25,4 +30,23 @@ def transcribe(audio_data, b64=False):
         f.write(bin_data)
         transcript = model.transcribe(f.name)
         logger.info(transcript)
+
         return json.dumps({'transcription': transcript})
+
+
+@task_prerun.connect
+def task_prerun_handler(task_id, task, **extras):
+    """ Dispatched before a task is executed. """
+    tasks_exec_time[task_id] = time.perf_counter()
+
+
+@task_postrun.connect
+def task_postrun_handler(task_id, task, **extras):
+    """ Dispatched after a task has been executed. """
+    end = time.perf_counter()
+    elapsed_time = end - tasks_exec_time.pop(task_id)
+
+    delay = 7
+    with open(f'performance_{delay}secs.txt', 'a') as f:
+        f.write(str(elapsed_time) + '\n')
+    logger.info(f'Task {task_id} took {str(round(elapsed_time, 3))} seconds.')
